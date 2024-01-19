1fb61bc92a76114b967ded266a769df32fdb31db
Sushovan <sushovanghosh960@gmail.com>
2024-01-19 19:09:35 +0530

    PlayingButton and UploadButton passes event handler as props to Button Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,24 @@ import Profile from './components/Profle';
 import Button from './components/Button';
 import './App.css'
 
+function PlayingButton() {
+  return (
+    <Button eventHandler={() => alert('Playing')}>Play</Button>
+  )
+};
+
+function UploadButton() {
+  return (
+    <Button eventHandler={() => alert('Uploading')}>Upload</Button>
+  )
+};
+
 function App() {
   return (
-    <Button />
+    <>
+      <PlayingButton />
+      <UploadButton />
+    </>
   )
 }
 
