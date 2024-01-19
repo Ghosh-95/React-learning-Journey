eb706184feb5b027d03f1dc054a6c2b7338dbfba
Sushovan <sushovanghosh960@gmail.com>
2024-01-19 19:35:31 +0530

    add e.stopPropagation to stop the event propagation.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
 export default function Button({ eventHandler, children }) {
-
+    // We can wrap the eventHandler prop and add as many line of code, then we can CALL the eventHandler function (prop)
     return (
         <>
-            <button onClick={eventHandler}>{children}</button>
+            <button onClick={(e) => {
+                e.stopPropagation();
+                eventHandler();
+            }}>{children}</button>
         </>
     )
 }
