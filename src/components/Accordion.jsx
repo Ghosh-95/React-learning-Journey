a8a71f36bd74ee05186da705a9d10efb7a0871bb
Sushovan <sushovanghosh960@gmail.com>
2024-04-05 13:27:33 +0530

    optimized accordion with 'lifting the state up' concept
    implement the state control functionality to Accordion component
    pass isActive prop to Panel component which will return either true or false.
    pass an onShow prop to Panel component which will be invoked on click of a button.
    now changing the state of the child components(Panel) is the responsibility of parent component(Accordion).

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,24 +1,32 @@
 import { useState } from "react"
 
-function Panel({ children, title }) {
-    const [isActive, setIsActive] = useState(false);
+function Panel({ children, title, isActive, onShow }) {
     return (
         <>
             <h1>{title}</h1>
             {isActive && <p>{children}</p>}
-            <button onClick={() => setIsActive(!isActive)}>{isActive ? "Hide" : "Show"}
+            <button onClick={onShow}>{isActive ? "Hide" : "Show"}
             </button>
         </>
     )
 }
 
 export default function Accordion() {
+    const [activeIndex, setActiveIndex] = useState(0);
     return (
         <>
-            <Panel title={"About"}>With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+            <Panel
+                isActive={activeIndex === 0}
+                onShow={() => setActiveIndex(0)}
+                title={"About"}>
+                With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
             </Panel>
 
-            <Panel title={"Entimology"}>The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+            <Panel
+                title={"Entimology"}
+                onShow={() => setActiveIndex(1)}
+                isActive={activeIndex === 1}>
+                The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
 
             </Panel>
         </>
